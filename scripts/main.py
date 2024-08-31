0225a34fe9da75e862d69c61a6ce8a1bcdc5ecbb
Ashan <apraba05@gmail.com>
2024-08-31 18:09:05 -0400

    feat: folder organization and separate asset based folder for img

---
+++ scripts/main.py
@@ -0,0 +1,30 @@
+from PIL import ImageTk, Image
+import os
+import pytesseract
+import PIL.Image
+
+def openFileExplorer():
+   global fileName,img,info,fileExact
+   fileName = filedialog.askopenfilename(initialdir= "./PyCo/assets",title = "Select A File",filetypes = (("png files","*.png"),("all files","*.*"),("jpeg files","*.jpeg"),("jpg files","*.jpg")))
+   img = Image.open(fileName)
+   fileExact = os.path.basename(fileName).split('/')[-1]
+   result = messagebox.askyesno(
+      title = 'Yes or No',
+      message = "Please Confirm if this is the image you would like to upload.",
+      detail = fileExact
+   )
+   if result:   
+            try:
+                info = pytesseract.image_to_string(PIL.Image.open(fileName),config=r"--psm 6 --oem 3")
+                compiled_code = compile(info, '<string>', 'exec')
+                exec(compiled_code)
+            except Exception as e:
+                newWindow = Toplevel()
+                newWindow.geometry("500x400+600+300")
+                errorLabel = CTkLabel(master= newWindow, text="Error Executing Python Code", font=("Arial", 20))
+                errorLabel.pack(pady=20, padx=10)
+
+app.mainloop()
+
+
+
